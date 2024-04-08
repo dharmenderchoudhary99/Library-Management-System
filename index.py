0fbd085e0f2b018cbc94672aa99f862893939209
dharmenderchoudhary99 <dharmenderchoudhary022@gmail.com>
2024-04-08 13:11:31 +0530

    Updated index.py

--- index.py
+++ index.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+
 from routes.student import student
 
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
+
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return RedirectResponse(url="/docs")
 
 app.include_router(student)
